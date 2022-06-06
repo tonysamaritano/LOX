80ecbc43a8b6072bd11aea109916b90c3f81254c
Tony Samaritano <tony@vergeaero.com>
2022-06-06 10:01:15 -0400

    updates test_token.py

--- testing/test_token.py
+++ testing/test_token.py
@@ -6,4 +6,6 @@ from pylox.token import Token, TokenType
 def test_token():
     token = Token(TokenType.DOT, ".", 1337)
     assert("[1337] . -> TokenType.DOT" == str(token))
-    assert("[no.lox:1234] ... -> TokenType.WHILE" != str(token))
+    assert("[1336] . -> TokenType.DOT" != str(token))
+    assert("[1337] , -> TokenType.DOT" != str(token))
+    assert("[1337] . -> TokenType.WHILE" != str(token))
